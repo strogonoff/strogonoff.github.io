6a5d2f486ae719b3bd33a26e699ce77240951d2c
Anton Strogonoff <anton@strogonoff.name>
2017-04-09 17:34:10 +0700

    Next.js

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+    <div>
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Anton Strogonoff</title>
+        </Head>
+
+        <h1>
+            Simpler is better.
+        </h1>
+
+        <p>
+            Previously contracted independently for:
+        </p>
+
+        <ul>
+            <li>
+                Upwork Global
+            </li>
+            <li>
+                Solid Logic Technology
+            </li>
+        </ul>
+    </div>
+)
